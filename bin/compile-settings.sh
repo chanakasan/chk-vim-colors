a66e82f892ac00e36571c5af76e6f55125e3baf0
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-17 01:39:11 +0530

    first

---
+++ bin/compile-settings.sh
@@ -0,0 +1,25 @@
+main() {
+  eco "hi"
+}
+
+read_fio "hi"
+}
+
+read_fio "hi"
+}
+
+read_file() {
+    echo he() {
+    echo he() {
+    echo ho "hi"
+}
+
+read_file() {
+  echo hi
+}
+
+extract_text() {
+  echo hi
+}
+
+main
